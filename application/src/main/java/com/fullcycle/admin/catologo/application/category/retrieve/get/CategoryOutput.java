790c21aec8def6c7c95c4e61d41db668acceed0f
marciodev2000 <marciodev2000@gmail.com>
2025-07-18 14:33:37 +0100

    UseCases concluido com sucesso

---
+++ application/src/main/java/com/fullcycle/admin/catologo/application/category/retrieve/get/CategoryOutput.java
@@ -0,0 +1,4 @@
+package com.fullcycle.admin.catologo.application.category.retrieve.get;
+
+public record CategoryOutput() {
+}
---
+++ application/src/main/java/com/fullcycle/admin/catologo/application/category/retrieve/list/DefaultListCategoriesUseCase.java
@@ -0,0 +1,4 @@
+package com.fullcycle.admin.catologo.application.category.retrieve.list;
+
+public class DefaultListCategoriesUseCase {
+}
